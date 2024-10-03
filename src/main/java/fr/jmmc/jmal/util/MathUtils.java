4a4cb9fa37b4c9b5e93266a5a95e400a39770b83
bourgesl <bourges.laurent@gmail.com>
2024-10-03 17:20:08 +0200

    disable atan2 checks (slow and more confusing)

--- src/main/java/fr/jmmc/jmal/util/MathUtils.java
+++ src/main/java/fr/jmmc/jmal/util/MathUtils.java
@@ -3,9 +3,6 @@
  ******************************************************************************/
 package fr.jmmc.jmal.util;
 
-import static fr.jmmc.jmcs.util.NumberUtils.ARG_EPSILON;
-import static fr.jmmc.jmcs.util.NumberUtils.PI;
-import static fr.jmmc.jmcs.util.NumberUtils.PI_HALF;
 import net.jafama.FastMath;
 
 /**
@@ -20,25 +17,6 @@ public final class MathUtils {
     }
 
     public static double getArgument(final double re, final double im) {
-        // check |re| == 0
-        final double normRe = Math.abs(re);
-        if (normRe == 0.0) {
-            return (im >= 0.0) ? PI_HALF : -PI_HALF;
-        }
-        // check |im| == 0
-        final double normIm = Math.abs(im);
-        if (normIm == 0.0) {
-            return (re >= 0.0) ? 0.0 : PI;
-        }
-        final double epsilon = ARG_EPSILON * (normRe + normIm);
-        // check |re| << |im| or |im| << |re|
-        if (normIm <= epsilon) {
-            return (re >= 0.0) ? 0.0 : PI;
-        }
-        // check |im| << |re|
-        if (normRe <= epsilon) {
-            return (im >= 0.0) ? PI_HALF : -PI_HALF;
-        }
         return FastMath.atan2(im, re);
     }
 
